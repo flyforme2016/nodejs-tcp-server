d733c57067e49e1d8621e626843fcaaf77b34c76
dlsgk2016 <cjdfidrlwjd@naver.com>
2024-01-16 12:07:26 +0900

    :recycle: 리팩토링
    fooHandler 함수명 변경에 따라 호출명 변경
    determineHost 함수 주석 추가
    
    Message footer

--- dispatch-host.js
+++ dispatch-host.js
@@ -1,11 +1,11 @@
 const { requestParser } = require("./request-parser");
-const { fooHandler } = require("./foo/handler");
+const { handleFooRequest } = require("./foo/handler");
 const { barHandler } = require("./bar/handler");
 const { response404 } = require("./common/error");
 const config = require("./config.json");
 const hostHandlerMap = new Map();
 
-hostHandlerMap.set("www.foo.com", fooHandler);
+hostHandlerMap.set("www.foo.com", handleFooRequest);
 hostHandlerMap.set("www.bar.com", barHandler);
 
 /**
@@ -26,7 +26,13 @@ function dispatchHost(socket, buffer) {
   }
 }
 
+/**
+ * 요청의 Host 헤더를 분석하여 host를 반환하는 함수
+ * @param {string} host "www.foo.com"
+ * @returns {string} host "www.bar.com"
+ */
 function determineHost(host) {
+  // 1) 요청 헤더에 host가 작성되어 있지 않은 경우 default host(www.foo.com)사용.
   return host === "127.0.0.1" ? config.defaultHost : host;
 }
 
