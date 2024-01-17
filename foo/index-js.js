ac77b5ac34e00ec97e36750986a6c050dd833d0c
dlsgk2016 <cjdfidrlwjd@naver.com>
2024-01-17 17:29:30 +0900

    :sparkles: index.html에 대한 css 및 js 추가
    index.html에서 사용할 외부 css 및 js 파일 생성
    css 및 js 리소스에 요청에 대한 handler 구현

---
+++ foo/index-js.js
@@ -0,0 +1,26 @@
+const { httpResponse } = require("../common/http-response");
+const { serializeHeaders } = require("../common/serialize-header");
+const { headers } = require("../common/response-header");
+const fs = require("fs");
+const OK_STATUS_CODE = 200;
+/**
+ * 응답 본문 및 헤더를 생성하고 HTTP 응답을 전송하는 httpResponse()를 호출하는 서비스.
+ * @param {net.socket} socket
+ * @param {object} request { method: 'POST', path: '/test.html', version: 'HTTP/1.1', header: [ 'Host: www.bar.com', ...], body: [ '' ] }
+ */
+function fooIndexJs(socket, request, filePath) {
+  const stat = fs.statSync(filePath);
+  const file = fs.readFileSync(filePath);
+  const fileSize = stat.size;
+
+  const responseHeader = {
+    ...headers,
+    "Content-Length": fileSize,
+    "Content-Type": "text/html; charset=UTF-8",
+    "Last-Modified": stat.mtime.toUTCString(),
+  };
+  const headerString = serializeHeaders(OK_STATUS_CODE, responseHeader);
+  httpResponse(socket, headerString, file);
+}
+
+module.exports = { fooIndexJs };
--- foo/service-map.js
+++ foo/service-map.js
@@ -1,11 +1,14 @@
 const { fooIndex } = require("./index");
 const { fooTest } = require("./test");
 const { redirectRequest } = require("../common/redirect");
-
+const { fooIndexJs } = require("./index-js");
+const { fooIndexCss } = require("./index-css");
 const serviceMap = new Map([
   ["/index.html", fooIndex],
   ["/test.html", fooTest],
   ["/redirect", redirectRequest],
+  ["/index-js.js", fooIndexJs],
+  ["/index-css.css", fooIndexCss],
 ]);
 
 module.exports = { serviceMap };
