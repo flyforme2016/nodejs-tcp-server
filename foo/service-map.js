fd2f82fa1ff98dbf8fa8301b075f55cdfaae2bf2
dlsgk2016 <cjdfidrlwjd@naver.com>
2024-01-22 12:23:31 +0900

    :sparkles: origin.js 구현
    /origin.jpg 경로로 요청 시 해당 이미지 리소스를 반환하는 기능 구현

--- foo/service-map.js
+++ foo/service-map.js
@@ -4,6 +4,7 @@ const { fooTravel } = require("./travel");
 const { redirectRequest } = require("../common/redirect");
 const { fooIndexJs } = require("./index-js");
 const { fooIndexCss } = require("./index-css");
+const { fooImage } = require("./image");
 const serviceMap = new Map([
   ["/index.html", fooIndex],
   ["/test.html", fooTest],
@@ -11,6 +12,7 @@ const serviceMap = new Map([
   ["/redirect", redirectRequest],
   ["/index-js.js", fooIndexJs],
   ["/index-css.css", fooIndexCss],
+  ["/origin.jpg", fooImage],
 ]);
 
 module.exports = { serviceMap };
