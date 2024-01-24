1302e0cb3a3a92dafd39dfe4628081cc88cc0361
dlsgk2016 <cjdfidrlwjd@naver.com>
2024-01-24 18:56:50 +0900

    :sparkles: pagedesk 기능 개발
    pagedesk의 replace, html, dom, refctrl 기능 구현

---
+++ foo/page/pagedesk/page-desk-dom-tags.js
@@ -0,0 +1,30 @@
+const { httpResponse } = require("../../../common/http-response");
+const { buildResponseHeaderBuffer } = require("../../../common/response-header-buffer-builder");
+const { headers } = require("../../../common/response-header");
+const fs = require("fs");
+const { compressHttp } = require("../../../common/http-compressor");
+const { combineBuffer } = require("../../../common/bufferCombiner");
+const OK_STATUS_CODE = 200;
+/**
+ * 응답 본문 및 헤더를 생성하고 HTTP 응답을 전송하는 httpResponse()를 호출하는 서비스.
+ * @param {net.socket} socket
+ * @param {object} request { method: 'POST', path: '/test.html', version: 'HTTP/1.1', header: [ 'Host: www.bar.com', ...], body: [ '' ] }
+ */
+function pageDeskDomTags(socket, request, filePath) {
+  const stat = fs.statSync(filePath);
+  const file = fs.readFileSync(filePath);
+  const fileSize = stat.size;
+
+  const responseHeader = {
+    ...headers,
+    "Content-Length": fileSize,
+    "Content-Type": "text/html; charset=UTF-8",
+    "Last-Modified": stat.mtime.toUTCString(),
+  };
+
+  const headerBuffer = buildResponseHeaderBuffer(OK_STATUS_CODE, responseHeader);
+  const response = combineBuffer(headerBuffer, file);
+  httpResponse(socket, response);
+}
+
+module.exports = { pageDeskDomTags };
--- foo/service-map.js
+++ foo/service-map.js
@@ -6,6 +6,12 @@ const { fooIndexJs } = require("./index-js");
 const { fooIndexCss } = require("./index-css");
 const { fooImage } = require("./image");
 const { pageFx } = require("./pagefx");
+const { pageDeskPre } = require("./page/pagedesk/pagedesk-pre");
+const { pageDeskHtmlIn } = require("./page/pagedesk/page-desk-html-in");
+const { pageDeskDomInsert } = require("./page/pagedesk/page-desk-dom-insert");
+const { pageDeskDomRemove } = require("./page/pagedesk/page-desk-dom-remove");
+const { pageDeskDomTags } = require("./page/pagedesk/page-desk-dom-tags");
+const { pageDeskDomRefctrl } = require("./page/pagedesk/page-desk-dom-refctrl");
 const serviceMap = new Map([
   ["/index.html", fooIndex],
   ["/test.html", fooTest],
@@ -18,6 +24,12 @@ const serviceMap = new Map([
   ["/origin3.jpg", fooImage],
   ["/origin4.jpg", fooImage],
   ["/page-fx.html", pageFx],
+  ["/page-desk-pre.html", pageDeskPre],
+  ["/page-desk-html-in.json", pageDeskHtmlIn],
+  ["/page-desk-dom-insert.html", pageDeskDomInsert],
+  ["/page-desk-dom-remove.html", pageDeskDomRemove],
+  ["/page-desk-dom-tags.html", pageDeskDomTags],
+  ["/page-desk-dom-refctrl.html", pageDeskDomRefctrl],
 ]);
 
 module.exports = { serviceMap };
